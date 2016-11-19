65a850bec10e000a6469c052a01facd3b9643ade
Aaron Furtado <aaronfurtado93@hotmail.com>
2016-11-19 13:00:41 +0530

    Styled Laps

--- app/components/LapTime.js
+++ app/components/LapTime.js
@@ -4,17 +4,31 @@ import React, {Component} from 'react';
 
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native';
 
 export default class LapTime extends Component {
   render() {
     return(
-      <View style={this.props.style}>
-        <Text>
-          #{this.props.id} {formatTime(this.props.lapTime)}
+      <View style={style.lapContainer}>
+        <Text style={style.lapText}>
+          Lap #{this.props.id + 1}
+        </Text>
+        <Text style={style.lapText}>
+          {formatTime(this.props.lapTime)}
         </Text>
       </View>
     )
   }
 }
+
+const style = StyleSheet.create({
+  lapContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  lapText: {
+    fontSize: 30,
+  }
+})
